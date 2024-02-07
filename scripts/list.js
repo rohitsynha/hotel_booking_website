d80a91cd818f672860c12ec01587cdae3c1ede0b
Rohit Sinha <rohit@adeptmind.ai>
2024-02-08 05:07:00 +0530

    Checkpoint 9

--- scripts/list.js
+++ scripts/list.js
@@ -1,25 +1,21 @@
 
-
-function _selectTab() {
-    const selectedClass = "btn-primary";
-    const btn = $(this);
-    const id = btn.attr("id");
-    const viewId = id.split('-').slice(0, -1).join('-');
-
-    if (btn.hasClass(selectedClass)) {
-        return;
-    }
-
-    $('.views-list-item').removeClass(selectedClass);
-
-    $('.view-content').hide();
-    $(`#${viewId}`).show();
-
-    btn.addClass(selectedClass);
-}
-
-function selectTab() {
-    $('.views-list-item').click(_selectTab());
-}
-
-$(selectTab());
+$(function() {
+    $('.views-list-item').click(function() {
+        const selectedClass = "btn-primary";
+        const btn = $(this);
+        const id = btn.attr("id");
+        console.log(id)
+        const viewId = id.split('-').slice(0, -1).join('-');
+    
+        if (btn.hasClass(selectedClass)) {
+            return;
+        }
+    
+        $('.views-list-item').removeClass(selectedClass);
+    
+        $('.view-content').hide();
+        $(`#${viewId}`).show();
+    
+        btn.addClass(selectedClass);
+    });
+});
